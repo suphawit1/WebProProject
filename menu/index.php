4e1d20c236f82ee25fb95340e59bc802114dddeb
Suphawit Pattamapassakun <112681287+suphawit1@users.noreply.github.com>
2024-02-22 10:20:40 +0000

    Add files via upload

--- menu/index.php
+++ menu/index.php
@@ -15,13 +15,18 @@
         // Create connection
         $conn = mysqli_connect($servername, $username, $password, $dbname);
         // Check connection
-        if (!$conn) {
-          die("Connection failed: " . mysqli_connect_error());
+        $sql = "SELECT * FROM concerts;";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo " " . $row["concert_id"]. " " . $row["location"]. 
+            " " . $row["organizer_id"] . " " . $row["status"] . "<br>";
         }
-        echo "Connected successfully";
-        
-        // close connection
-        mysqli_close($conn);
+        } else {
+        echo "0 results";
+        }
     ?>
 </body>
 </html>
