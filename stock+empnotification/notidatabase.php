0e89218c49f8f52c552fe0324e33922150b94a4f
plamn147@gmail.com <plamn147@gmail.com>
2024-03-03 17:59:00 +0000

    Automatic commit for changes to file: notidatabase.php

---
+++ stock+empnotification/notidatabase.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "webpro.cpcueeyq8pic.us-east-1.rds.amazonaws.com";
+    $username = "admin"; //ตามที่กำหนดให้
+    $password = "nHINsFGvVfVxb1fc5Pyz"; //ตามที่กำหนดให้
+    $dbname = "ProjectDB";    //ตามที่กำหนดให้
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    $id = (int)$_POST['notify_id'];
+    $sql = "DELETE FROM emp_notify WHERE notify_id = $id;";
+    $result = mysqli_query($conn, $sql);
+    
+    
+?>
