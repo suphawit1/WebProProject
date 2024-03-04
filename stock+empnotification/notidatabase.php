1c11db3e12037d4a9c60e6b335bd41acc0b45d41
plamn147@gmail.com <plamn147@gmail.com>
2024-03-04 08:53:00 +0000

    Automatic commit for changes to file: notidatabase.php

--- stock+empnotification/notidatabase.php
+++ stock+empnotification/notidatabase.php
@@ -6,9 +6,16 @@
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    $id = (int)$_POST['notify_id'];
-    $sql = "DELETE FROM emp_notify WHERE notify_id = $id;";
-    $result = mysqli_query($conn, $sql);
-    
+    if (isset($_POST['notify_id'])){
+        $id = (int)$_POST['notify_id'];
+        $sql = "DELETE FROM emp_notify WHERE notify_id = $id;";
+        $result = mysqli_query($conn, $sql);
+    }else{
+        $sql = "SELECT COUNT(notify_id) FROM emp_notify";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $num = $row['COUNT(notify_id)'];
+        echo $num;
+    }
     
 ?>
