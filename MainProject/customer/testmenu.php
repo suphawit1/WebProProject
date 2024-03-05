2696cede94663e1c134b4e50d27eb52ec2f8440d
plamn147@gmail.com <plamn147@gmail.com>
2024-03-05 22:01:03 +0000

    Automatic commit for changes to file: testmenu.php

---
+++ MainProject/customer/testmenu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $servername = "webpro.cpcueeyq8pic.us-east-1.rds.amazonaws.com";
+        $username = "admin"; //ตามที่กำหนดให้
+        $password = "nHINsFGvVfVxb1fc5Pyz"; //ตามที่กำหนดให้
+        $dbname = "ProjectDB";
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+        $row = $_GET['row'];
+        $order = $_GET['menu'];
+
+        foreach (range(1, $row) as $i) {
+            if ($order[$i] != 0){
+                $sql = "SELECT name FROM menu WHERE menuid = $i;";
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                echo $row["name"]."จำนวน: ".$order[$i]." ที่<br>";
+            }
+        }
+    ?>
+</body>
+</html>
