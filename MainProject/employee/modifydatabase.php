17f902f6051be3b0360e3cfedd907368c241b99c
plamn147@gmail.com <plamn147@gmail.com>
2024-03-12 01:17:45 +0000

    Automatic commit for changes to file: modifydatabase.php

--- MainProject/employee/modifydatabase.php
+++ MainProject/employee/modifydatabase.php
@@ -5,11 +5,39 @@
     $dbname = "ProjectDB";    //ตามที่กำหนดให้
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-
+    session_start();
+    $nowsession = session_id();
+    $namesession = $_SESSION['name'];
+    $rolesession = $_SESSION['role'];
+    echo $namesession.$rolesession;
     if (isset($_POST['notify_id'])){
         $id = (int)$_POST['notify_id'];
+        $sql = "SELECT type,tableNo FROM emp_notify WHERE notify_id = $id";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $table = $row['tableNo'];
+        if ($row['type'] == "เรียกเก็บเงิน"){
+            $sql = "SELECT session_id FROM table_session WHERE table_no =$table";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)) {
+                $sessionid = $row['session_id'];
+                session_id($sessionid);
+                session_destroy();
+                $sql = "DELETE FROM table_session WHERE session_id = '$sessionid';";
+                mysqli_query($conn, $sql);
+            }
+            session_start();
+            $_SESSION['name'] = $namesession;
+            $_SESSION['role'] = $rolesession;
+            echo $namesession.$rolesession;
+            $sql = "UPDATE cus_table SET amount=0, stutus=0 WHERE table_id = $table";
+            mysqli_query($conn, $sql);
+            $sql = "DELETE FROM menu_order WHERE table_no = $table";
+            mysqli_query($conn, $sql);
+        }
         $sql = "DELETE FROM emp_notify WHERE notify_id = $id;";
         $result = mysqli_query($conn, $sql);
+        
     }else if (isset($_POST['menu_id'])){
         $id = (int)$_POST['menu_id'];
         if ((int)$_POST['type'] == 0){
@@ -25,5 +53,4 @@
         $num = $row['COUNT(notify_id)'];
         echo $num;
     }
-    
 ?>
